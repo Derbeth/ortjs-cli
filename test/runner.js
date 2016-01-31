c7facf7d202fdce7034c0a0dd5cf04bcc70c8d16
Derbeth <derbeth@interia.pl>
2016-01-31 19:31:01 +0100

    Handle Windows encoding and end-of-line
    Use iconv-lite

--- test/runner.js
+++ test/runner.js
@@ -11,23 +11,23 @@ describe("runner", function() {
         });
 
         it("saves corrected input", function(done) {
-            fs.writeFileSync('/tmp/test-in.txt', "4-ro godzinny\n8-mio osobowy\n", 'utf8');
+            fs.writeFileSync('/tmp/test-in.txt', "4-ro letni książe\n8-mio osobowy\n", 'utf8');
             runner('/tmp/test-in.txt', '/tmp/test-out.txt', {}, function() {
                 expect(fs.readFileSync('/tmp/test-out.txt', {encoding: 'utf8'}))
-                    .to.equal("4-godzinny\n8-osobowy\n");
+                    .to.equal("4-letni książę\n8-osobowy\n");
                 done();
             });
 
         });
     });
 
-    xdescribe("using Windows EOL and encoding", function() {
+    describe("using Windows EOL and encoding", function() {
         afterEach(function() {
             fs.unlink('/tmp/test-out2.txt');
         });
 
         it("maintains EOL and encoding", function(done) {
-            runner('test/win-input.txt', '/tmp/test-out2.txt', {encoding: 'CP1250'}, function() {
+            runner('test/win-input.txt', '/tmp/test-out2.txt', {encoding: 'cp1250'}, function() {
                 exec('diff -u /tmp/test-out2.txt test/win-output.txt', {}, (error, stdout) => {
                     if (error) {
                         throw new Error(`Diffing failed\n${stdout}`);
