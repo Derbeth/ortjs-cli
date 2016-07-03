f3eae74dbd4f9700efd85c7bfeda2d186c933687
Derbeth <derbeth@interia.pl>
2016-07-03 11:59:10 +0200

    Accept options for OrtJS

--- test/cli.js
+++ test/cli.js
@@ -5,6 +5,11 @@ const runOrt = function (args) {
     return execSync('bin/ortjs ' + args, {encoding: 'utf8'});
 }
 
+var runOrtForInput = function(input, args) {
+    args = args || '';
+    return execSync(`echo "${input}" | bin/ortjs ${args} - -`, {encoding: 'utf8'});
+}
+
 describe("bin/ortjs", function() {
     describe("--help", function() {
         it("shows some help", function() {
@@ -13,8 +18,14 @@ describe("bin/ortjs", function() {
     });
     describe("from stdin to stdout", function() {
         it("prints corrected input line by line", function() {
-            expect(execSync('echo "4-ro godzinny\n8-mio osobowy" | bin/ortjs - -', {encoding: 'utf8'}))
+            expect(runOrtForInput('4-ro godzinny\\n8-mio osobowy'))
                 .to.equal("4-godzinny\n8-osobowy\n");
+        });
+    });
+    describe("--typography", function() {
+        it("is accepted", function() {
+            expect(runOrtForInput('a - b', '--no-typography'))
+                .to.equal("a - b\n");
         });
     });
 });
